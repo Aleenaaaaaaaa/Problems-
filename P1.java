b3ca92bb03db65b6fd5d27d4537dbf622d174174
Aleena sunny <aleena0416@gmail.com>
2025-09-19 11:12:52 +0530

    Create P1.java

---
+++ P1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt(); // number of integers
+        
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            System.out.println(num);
+        }
+        
+        sc.close();
+    }
+}
